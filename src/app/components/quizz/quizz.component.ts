188b76f486255a52a9c964028806d2d9884ad1ad
Lucas Goulart <lucas.goulart@wearedev.com.br>
2023-12-13 22:08:58 -0300

    :tada: Project Start: BuzzFeed Quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
 
+
+export class QuizzComponent implements OnInit{
+  title:string = ""
+
+  questions:any = ""
+  questionSelected:any = ""
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = true
+
+  constructor(){}
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+  }
+
+  selectedOption (value:string) {
+    if(this.questionMaxIndex > this.questionIndex){
+      this.answers.push(value)
+      this.nextStep()
+    }
+  }
+
+  nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+
+    console.log(this.answers)
+  }
 }
